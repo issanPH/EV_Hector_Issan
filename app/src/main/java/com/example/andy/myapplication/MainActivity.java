b884b023d716a20955e4ad503b7ba66f80bd9492
issanPH <56137098+issanPH@users.noreply.github.com>
2019-12-20 13:18:58 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/andy/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andy/myapplication/MainActivity.java
@@ -14,12 +14,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         final ListView list = findViewById(R.id.list);
         ArrayList<SubjectData> arrayList = new ArrayList<SubjectData>();
-        arrayList.add(new SubjectData("JAVA", "https://www.tutorialspoint.com/java/", "https://www.tutorialspoint.com/java/images/java-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Python", "https://www.tutorialspoint.com/python/", "https://www.tutorialspoint.com/python/images/python-mini.jpg"));
-        arrayList.add(new SubjectData("Javascript", "https://www.tutorialspoint.com/javascript/", "https://www.tutorialspoint.com/javascript/images/javascript-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cprogramming", "https://www.tutorialspoint.com/cprogramming/", "https://www.tutorialspoint.com/cprogramming/images/c-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Cplusplus", "https://www.tutorialspoint.com/cplusplus/", "https://www.tutorialspoint.com/cplusplus/images/cpp-mini-logo.jpg"));
-        arrayList.add(new SubjectData("Android", "https://www.tutorialspoint.com/android/", "https://www.tutorialspoint.com/android/images/android-mini-logo.jpg"));
+        arrayList.add(new SubjectData("PM", "4:00", ""));
+        arrayList.add(new SubjectData("PM", "5:00", ""));
+        arrayList.add(new SubjectData("AM", "6:00", ""));
+        arrayList.add(new SubjectData("AM", "6:30", ""));
+        arrayList.add(new SubjectData("AM", "7:00", ""));
+        arrayList.add(new SubjectData("PM", "12:00", ""));
         CustomAdapter customAdapter = new CustomAdapter(this, arrayList);
         list.setAdapter(customAdapter);
     }
